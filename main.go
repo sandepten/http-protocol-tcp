2d7f6f0d34a111d0a49f15f601e6529935d976c3
Sandeep Kumar <sandepten@gmail.com>
2025-03-25 12:50:14 +0530

    read full lines till end of line

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,12 +13,25 @@ func main() {
 	}
 	defer file.Close()
 
+	var line string
 	for {
 		byteArray := make([]byte, 8)
 		_, err := file.Read(byteArray)
 		if err != nil {
 			return
 		}
-		fmt.Printf("%s\n", string(byteArray))
+		readLine := string(byteArray)
+		if len(strings.Split(readLine, "\n")) == 1 {
+			line = line + readLine
+		} else {
+			for index, value := range strings.Split(readLine, "\n") {
+				if index == 0 {
+					fmt.Printf("read: %s\n", line+value)
+					line = ""
+					continue
+				}
+				line += value
+			}
+		}
 	}
 }
